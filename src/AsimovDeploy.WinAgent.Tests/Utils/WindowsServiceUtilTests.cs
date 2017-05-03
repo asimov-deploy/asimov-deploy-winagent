7d1a9feca379db65021f7e46bcbd690b7f48d561
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2017-05-03 14:50:54 +0200

    When retrieving windows service installation path we should handle an executable path not starting with double quotes

---
+++ src/AsimovDeploy.WinAgent.Tests/Utils/WindowsServiceUtilTests.cs
@@ -0,0 +1,24 @@
+﻿using AsimovDeploy.WinAgent.Framework.Common;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AsimovDeploy.WinAgent.Tests.Utils
+{
+    [TestFixture]
+    public class WindowsServiceUtilTests
+    {
+        [Test]
+        public void should_return_service_executable_for_path_starting_with_double_quote()
+        {
+            var serviceExe = WindowsServiceUtil.GetServiceExecutable(@"""C:\temp\service.exe"" -arg value");
+            serviceExe.ShouldBe(@"C:\temp\service.exe");
+        }
+
+        [Test]
+        public void should_return_service_executable_for_path_not_starting_with_double_quote()
+        {
+            var serviceExe = WindowsServiceUtil.GetServiceExecutable(@"C:\temp\service.exe -arg value");
+            serviceExe.ShouldBe(@"C:\temp\service.exe");
+        }
+    }
+}
